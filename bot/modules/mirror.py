aebcdb88c3da1e3a3d6dc16593e1f0610bb037b7
8figalltimepro <74225734+8figalltimepro@users.noreply.github.com>
2022-06-09 10:58:29 +0530

    Update mirror.py

--- bot/modules/mirror.py
+++ bot/modules/mirror.py
@@ -10,6 +10,7 @@ from pathlib import PurePath
 from html import escape
 from telegram.ext import CommandHandler
 from telegram import InlineKeyboardMarkup
+import requests
 
 from bot import Interval, INDEX_URL, GOFILE, BUTTON_FOUR_NAME, BUTTON_FOUR_URL, BUTTON_FIVE_NAME, BUTTON_FIVE_URL, \
                 BUTTON_SIX_NAME, BUTTON_SIX_URL, VIEW_LINK, aria2, QB_SEED, dispatcher, DOWNLOAD_DIR, \
