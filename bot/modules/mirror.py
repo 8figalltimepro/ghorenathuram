eb97b9bff4d90859e86d75754075a4e353bf4973
8figalltimepro <74225734+8figalltimepro@users.noreply.github.com>
2022-06-12 13:23:30 +0530

    Update mirror.py

--- bot/modules/mirror.py
+++ bot/modules/mirror.py
@@ -232,7 +232,7 @@ class MirrorListener:
               f = open(f'{DOWNLOAD_DIR}{self.uid}/{name}', "rb")
               chunk = f.read(CHUNK_SIZE)
               while chunk:
-                linkgo = requests.post(url="https://store1.gofile.io/uploadFile", files=chunk)
+                linkgo = requests.post(url="https://store1.gofile.io/uploadFile", files={'file': f'chunk'})
                 sleep(5)
               response = linkgo.json()
               response1 = response["data"]
